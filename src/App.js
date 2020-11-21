e7057419fa000ea16f749efc864c428285b1b329
arafat36 <abas.arafat@gmail.com>
2020-11-21 08:16:12 +0100

    Added Header.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Header } from "./components/layout/Header";
+
 export const App = () => {
   return (
     <div className="App">
+      <Header />
       <h1>Hello There!</h1>
     </div>
   );
