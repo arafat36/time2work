2c27ebb67e1e78f9c78efbaa1cfeea10bbba5dad
arafat36 <abas.arafat@gmail.com>
2020-12-18 09:54:54 +0100

    add AddTask component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Checkbox } from "./Checkbox";
+import { AddTask } from "./AddTask";
 import { useTasks } from "../hooks";
 import { collatedTasks } from '../constants';
 import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
@@ -37,6 +38,7 @@ export const Tasks = () => {
                     </li>
                 ))}
             </ul>
+            <AddTask />
         </div>
     );
 };
